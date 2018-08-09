08d2a046c5596469ea7ff3c17b56db2b44c31083
x <x>
2018-08-09 15:00:22 -0700

    Update normalization routine.

---
+++ README.md
@@ -0,0 +1,5 @@
+This project is 
+
+
+**normalize_data.py** - This both normalizes the format and the data.  In normalizing the format, it pickles an object containing data, target, feature_names, target_names.  It also goes through all the features and normalizes following page 56 of Cai 2013 - Maximizing Expected Model.
+
--- normalize_data.py
+++ normalize_data.py
@@ -8,20 +8,31 @@ import json
 
 # Load the diabetes dataset
 def main():
-    #convert("bike")
+    convert("bike")
     convert("concrete")
-    #convert("cps")
-    #convert("forestfires")
-    #convert("housing")
-    #convert("pm10")
-    #convert("winequality-red")
-    #convert("winequality-white")
+    convert("cps")
+    convert("forestfires")
+    convert("housing")
+    convert("pm10")
+    convert("redwine")
+    convert("whitewine")
 
 
 def convert(name):
+    """
+    Go through each file and change it to an object
+    containing data, target, feature_names, and target_names.
+    
+    Args:
+        name - Name of file before extension.
+    Return:
+        None
+    """
+    print()
+    print("Normalizing {}...".format(name))
     # Get meta data
     with open("data/{}.meta".format(name), "r") as infile:
-        column = int(json.loads(infile.read()))
+        column = int(infile.read())
     # Read header
     with open("data/{}.txt".format(name), "r") as infile:
         for line in infile:
@@ -34,24 +45,41 @@ def convert(name):
     header = np.array(header)
     m = list(range(x.shape[1]))
     del m[column]
-    print(m)
     a = {
-        'data': x[:, m],
+        'data': normalize(x[:, m]),
         'target': x[:, column],
         'feature_names': header[m],
         'target_names': header[column],
-        'DESCR': ''
     }
-    print(a)
     with open("data/{}.dat".format(name), "wb") as outfile:
         outfile.write(pickle.dumps(a))
 
 
-def converters(name):
-    if name == "bike":
-        return None
-    else:
-        return None
+def normalize(data):
+    """
+    Take feature set and normalize each column based
+    on min and max value (scale variable from 0 to 1).
+    
+    Args:
+        data - np matrix containing all features.
+    Return:
+        Data set such that all values are between 0 and 1.
+    """
+    for i in range(data.shape[1]):
+        x = data[:, i]
+        x_min = np.min(x)
+        x_max = np.max(x)
+        if x_min != x_max:
+            x = (x - x_min) / (x_max - x_min)
+            data[:, i] = x
+    # Check for NAN values.
+    for i in range(data.shape[0]):
+        for j in range(data.shape[1]):
+            if np.isnan(data[i, j]):
+                print(data[i, :])
+                exit()
+    return data
+
 
 if __name__ == "__main__":
     main()
